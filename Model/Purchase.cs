65841bb1d93c9ec3a66c7a787e9c198026e70b41
JoãoGabrielOlivera <joaogabrieloliveira81@gmail.com>
2020-05-28 21:31:12 -0300

    [ Back-end ] Criando tabela e Model para Compra(Purchase)

---
+++ Model/Purchase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Purchase
+    {
+        public int ID { get; set; }
+
+        public double TotalPrice;
+
+        public Usuario Usuario;
+        public List<Produto> Produtos;
+
+        public DateTime Created;
+
+        public Purchase()
+        {
+
+        }
+
+        public static Purchase FindByID()
+        {
+            return new Purchase();
+        }
+
+        public static Purchase FindByUserID(int id)
+        {
+            return new Purchase();
+        }
+
+        public static Purchase FindByUserID(Usuario user)
+        {
+            return new Purchase();
+        }
+    }
+}
--- Model/schema.sql
+++ Model/schema.sql
@@ -11,13 +11,6 @@ CREATE TABLE users(
 	created_at DATE
 )
 
-delete from users;
-
-INSERT INTO users (first_name, email,ecy_password,type_user,created_at) VALUES ('João', 'joao@gmail.com', '12345678','c',GETDATE());
-select * from users;
-select 0 from users WHERE id=5;
-drop table users;
-
 CREATE TABLE products(
 	id INT PRIMARY KEY identity NOT NULL,
 	product_name VARCHAR(60) NOT NULL,
@@ -27,10 +20,31 @@ CREATE TABLE products(
 	created_at DATE
 )
 
+CREATE TABLE purchases (
+	id INT NOT NULL,
+	total_price DECIMAL(7,2) NOT NULL,
+
+	user_id INT NOT NULL,
+
+	product_id INT,
+	created_at DATE
+)
+
+INSERT INTO users (first_name, email,ecy_password,type_user,created_at) VALUES ('João', 'joao@gmail.com', '12345678','c',GETDATE());
+
 INSERT INTO products (product_name, price,stock,suplier_id,created_at) VALUES ('Brinco', 10.00 , 10 , 2 ,GETDATE());
 INSERT INTO products (product_name, price,stock,suplier_id,created_at) VALUES ('Bolsa', 15.99 , 15 , 2 ,GETDATE());
 INSERT INTO products (product_name, price,stock,suplier_id,created_at) VALUES ('Turbante', 79.99 , 19 , 2 ,GETDATE());
 
+INSERT INTO purchases (id, total_price,user_id,product_id,created_at) VALUES (3, 10.00 , 1 , 3 ,GETDATE());
+INSERT INTO purchases (id, total_price,user_id,product_id,created_at) VALUES (3, 79.99 , 1 , 5 ,GETDATE());
+
+
+
 SELECT Top(2) products.ID,products.product_name,products.price,products.stock,products.created_at FROM products
 SELECT products.ID,products.product_name,products.price,products.stock,products.created_at FROM products
+
+SELECT * FROM users;
 SELECT * FROM products;
+
+SELECT purchases.id, purchases.total_price,users.first_name,purchases.product_id,purchases.created_at FROM purchases, users WHERE users.id = product_id
